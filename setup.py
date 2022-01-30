a6fbfe61ed546a7e2a587648565116f9be70e67b
David Nidever <dnidever@lsst.org>
2022-01-29 22:43:55 -0700

    moved directories

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@
 #from distutils.core import setup
 from setuptools import setup, find_packages
 
-setup(name='idl2pyr',
+setup(name='idl2py',
       version='1.0.0',
       description='Simple IDL to Python converter',
       author='David Nidever',
       author_email='dnidever@montana.edu',
       url='https://github.com/dnidever/idl2py',
       packages=find_packages(exclude=["tests"]),
+      package_dir={'idl2py':'python/idl2py'},
       scripts=['bin/idl2py'],
       include_package_data=True,
 )
