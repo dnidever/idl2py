20d99d1f35782d587afb0e3754f52e973987a963
David Nidever <dnidever@lsst.org>
2022-02-01 08:54:06 -0700

    bumped version from 1.0.0 to 1.0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(name='idl2py',
-      version='1.0.0',
+      version='1.0.1',
       description='Simple IDL to Python converter',
       author='David Nidever',
       author_email='dnidever@montana.edu',
